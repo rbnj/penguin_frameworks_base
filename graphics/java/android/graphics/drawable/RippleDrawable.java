3bef04899b82e973314ae93261875b38c979541f
aswin7469 <aswinas@pixysos.com>
2023-06-24 01:43:34 +0000

    graphics: Add back alpha clamping to ripple drawable
    This reverts commit 7b08444096e6344b9a1c5c888580251b2444c5ab.
    
    - Needed for our settings dashboard
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- graphics/java/android/graphics/drawable/RippleDrawable.java
+++ graphics/java/android/graphics/drawable/RippleDrawable.java
@@ -25,6 +25,7 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import android.animation.ValueAnimator;
+import android.annotation.ColorInt;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -992,9 +993,9 @@ public class RippleDrawable extends LayerDrawable {
         RippleShader shader = new RippleShader();
         // Grab the color for the current state and cut the alpha channel in
         // half so that the ripple and background together yield full alpha.
-        final int color = mMaskColorFilter == null
+        final int color = clampAlpha(mMaskColorFilter == null
                 ? mState.mColor.getColorForState(getState(), Color.BLACK)
-                : mMaskColorFilter.getColor();
+                : mMaskColorFilter.getColor());
         final int effectColor = mState.mEffectColor.getColorForState(getState(), Color.MAGENTA);
         final float noisePhase = AnimationUtils.currentAnimationTimeMillis();
         shader.setColor(color, effectColor);
@@ -1016,6 +1017,13 @@ public class RippleDrawable extends LayerDrawable {
         // Alpha is handled by the shader (and color is a no-op because there's a shader)
         p.setColor(0xFF000000);
         return properties;
+    }
+
+    private int clampAlpha(@ColorInt int color) {
+        if (Color.alpha(color) < 128) {
+            return  (color & 0x00FFFFFF) | 0x80000000;
+        }
+        return color;
     }
 
     @Override
@@ -1232,7 +1240,7 @@ public class RippleDrawable extends LayerDrawable {
 
         // Grab the color for the current state and cut the alpha channel in
         // half so that the ripple and background together yield full alpha.
-        final int color = mState.mColor.getColorForState(getState(), Color.BLACK);
+        final int color = clampAlpha(mState.mColor.getColorForState(getState(), Color.BLACK));
         final Paint p = mRipplePaint;
 
         if (mMaskColorFilter != null) {
